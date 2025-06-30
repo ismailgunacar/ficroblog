f579884bd375bb3823e578e24c069e640fadbbd9
Marka Gunacar <marka@gunac.ar>
2025-06-29 21:22:36 -0400

    delete

--- src/utils.ts
+++
@@ -1,4 +0,0 @@
-export function getDomainFromRequest(c: { req: { header: (name: string) => string | undefined } }): string {
-  const host = c.req.header('host') || c.req.header('x-forwarded-host') || 'localhost:8000';
-  return host;
-} 
